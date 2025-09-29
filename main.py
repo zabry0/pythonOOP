6a38f47857fa94afa689f32ca37f813e4fd130e1
Ondřej Zábrodský <Ondrej.Zabrodsky@student.souepl.cz>
2025-09-29 09:09:18 +0200

    29.9.2025 commit

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-class Car:
-    def __init__(self, znacka: str, rok: int, model: str, barva: str, typ_prevodovky: str, cena: float):
-        self.znacka = znacka
-        self.rok = rok
-        self.model = model
-        self.barva = barva
-        self.typ_prevodovky = typ_prevodovky
-        self.cena = cena
 
-    def cena_bez_dph(self):
+class Car:
+    def __init__(
+        self,
+        znacka: str,
+        rok: int,
+        model: str,
+        barva: str,
+        typ_prevodovky: str,
+        cena: float
+    ):
+        self.znacka: str = znacka
+        self.rok: int = rok
+        self.model: str = model
+        self.barva: str = barva
+        self.typ_prevodovky: str = typ_prevodovky
+        self.cena: float = cena
+
+
+    def cena_bez_dph(self) -> float:
         return self.cena / 1.21
 
-    def Vypis(self):
+
+    def Vypis(self) -> str:
         return f"""--- Auto ---
 Značka: {self.znacka}
 Rok výroby: {self.rok}
@@ -20,10 +31,24 @@ Typ převodovky: {self.typ_prevodovky}
 Cena bez DPH: {self.cena_bez_dph():.2f} Kč
 """
 
+
 audi = Car("Audi", 1999, "A4", "stříbrná", "Manuální", 45000)
 skoda = Car("Škoda", 2014, "Superb II", "Bílá", "Manuální", 31000)
-bmw = Car("BMW", 2020, "X5", "Černá", "Automatická", 1200000)
 
-print(audi.Vypis())
-print(skoda.Vypis())
-print(bmw.Vypis())
+
+auto = [
+    Car("Audi", 1999, "A4", "stříbrná", "Manuální", 45000),
+    Car("Škoda", 2014, "Superb II", "Bílá", "Manuální", 31000),
+    Car("BMW", 2020, "X5", "Černá", "Automatická", 1200000),
+    Car("Ford", 2018, "Focus", "Modrá", "Manuální", 250000),
+    Car("Toyota", 2021, "Corolla", "Červená", "Automatická", 600000),
+    Car("Honda", 2017, "Civic", "Šedá", "Manuální", 300000),
+    Car("Mercedes", 2019, "C-Class", "Bílá", "Automatická", 800000),
+    Car("Volkswagen", 2015, "Golf", "Zelená", "Manuální", 200000),
+    Car("Nissan", 2022, "Altima", "Modrá", "Automatická", 700000),
+    Car("Hyundai", 2016, "Elantra", "Stříbrná", "Manuální", 220000)
+]
+
+
+print(auto.Vypis())
+
